de103fdc318f309b3eaabe9293e454dc2c5872f8
pedro <pedrosiracusa@gmail.com>
2015-04-13 14:25:32 -0300

    Second exercise completed

--- atividades/ses_2/090030711_ex2.c
+++ atividades/ses_2/090030711_ex2.c
@@ -1,9 +1,27 @@
-Cabeçalho: 
+/*
+    Arquivo: 090030711_ex2.c
+    Computação Básica - Turma G
+    Nome: Pedro de Siracusa
+    Matrícula: 09/0030711
+    Descrição: 
+*/
 
-  /*
-      Arquivo: 090030711-Ex.(x).c
-      Computação Básica - Turma G
-      Nome: Pedro de Siracusa
-      Matrícula: 09/0030711
-      Descrição: (xxxx)
-  */
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    printf("Informe o primeiro valor: ");
+    scanf("%d", &a);
+    printf("Informe o segundo valor: ");
+    scanf("%d", &b);
+    if (a==b){
+        printf("Os números inseridos são iguais: %d\n", a);
+    }
+    else if (a>b){
+        printf("O maior número é %d\n", a);
+    }
+    else{ /* case where b is greater than a */
+        printf("O maior número é %d\n", b);
+    }
+    return(0);
+}
